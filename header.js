13c4e7518f1991752b904a42d0d976e282fafecf
Carlos <casalgado86@gmail.com>
2023-04-27 17:17:55 -0500

    fixed nav animation

--- header.js
+++ header.js
@@ -1,13 +1,13 @@
-const menu__button = document.getElementById("menu__button")
-const nav__small = document.getElementById("nav__small")
+const menu__button = document.getElementById("menu__button");
+const nav__small = document.getElementById("nav__small");
 
 menu__button.addEventListener("click", slideIn);
-nav__small.addEventListener ("mouseleave", slideOut);
+nav__small.addEventListener("mouseleave", slideOut);
 
 function slideIn() {
-    nav__small.style.transform = "translateX(0%)"
+  nav__small.style.width = "200px";
 }
 
 function slideOut() {
-    nav__small.style.transform = "translateX(100%)"
+  nav__small.style.width = "0px";
 }
